a581c72429c61accc226b27e67d3a5c1c45c76b5
Sayuri Cero <Hisaalqaoud@gmail.com>
2025-08-26 20:09:12 +0300

    hidden db url

--- src/databse.ts
+++ src/databse.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import { env } from './config/env';
+
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://sayuricerojp:biONHXkXy8yrIYeJ@cluster0.z6qhfos.mongodb.net/");
-        console.log("Connected to MongoDB");
+        const conn = await mongoose.connect(env.DB_URL);
+        console.log(`Connected to MongoDB: ${conn.connection.host}`);
     } catch (error) {
         console.error("Failed to connect to MongoDB", error);
     }
